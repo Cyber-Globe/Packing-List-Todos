b7b976f3e8eb1673f44772ac4a98ff7d681659fd
cyber <ojeikelvin@gmail.com>
2025-09-01 03:28:05 +0100

    added the function of the stats components

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,8 +1,14 @@
-export default function Stats() {
+export default function Stats({ items }) {
+  const numItems = items.length;
+  const numPack = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPack / numItems) * 100);
   return (
     <>
       <footer className="stats">
-        <em>you have X items on your List and you have already packed X</em>
+        <em>
+          you have {numItems} items on your List and you have already packed
+          {numPack} Items and {percentage}% Total was Packed
+        </em>
       </footer>
     </>
   );
